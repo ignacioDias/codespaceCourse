63862c017cc08f0ccea74f4c09e3f86c239e5862
Ignacio Dias Gundin <102194047+ignacioDias@users.noreply.github.com>
2023-09-09 22:49:49 +0000

    cambios finales espero

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/ines-alvarez.jpg";
+import image from "../images/james-harrison.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
